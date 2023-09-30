f56d1bedbd66c892435e5188c83fa757e298bf8e
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-09-30 22:44:59 +0800

    update 23.09.30

---
+++ src/components/Content.jsx
@@ -0,0 +1,45 @@
+export default function Content() {
+    return (
+
+        <div className="main-container">
+            <div className="container">
+                <div className="banner mb-30">
+                    <img src="public/img/banner.jpg" alt="banner" className="border-r" />
+                </div>
+                <div className="row">
+                    <div className="left mb-30">
+                        <div className="head-title border-r ">
+                            Vegetables
+                        </div>
+                        <div className="photo-grid">
+                            <ul>
+                                <li className="border-r ">
+                                    <img src="public/img/vegetable01.jpg" alt="Vegetables" />
+                                </li>
+                                <li className="border-r ">
+                                    <img src="public/img/vegetable02.jpg" alt="Vegetables" />
+                                </li>
+                                <li className="border-r ">
+                                    <img src="public/img/vegetable03.jpg" alt="Vegetables" />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="right mb-30">
+                        <div className="head-title border-r ">
+                            Contact
+                        </div>
+                        <div className="card">
+                            <p>For any questions or suggestions about Vegetables, Vegetables Club or your
+                                online order you can contact Vegetables Customer Service by phone, email
+                                or post and we’ll be happy to help.</p>
+                            <hr />
+                            <p className="lineH-2">E-mail : Vegetables@aaabbccc.com<br />
+                                PHONE : +886-123-456-789</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer";
+import Content from "../components/Content";
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
